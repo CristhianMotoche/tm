13fd280761e83c612952feebd7b99a38cc73695f
CristhianMotoche <cristhian.motoche@gmail.com>
2025-05-01 21:15:53 -0500

    chore: Attempt to get tags

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,37 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
-import System.Environment (getArgs)
-import System.Exit (die)
-import System.FilePath
-import System.Directory (doesFileExist, renameFile)
-import qualified Data.Text as T
-import Control.Monad (unless)
-import Control.Monad.Trans.Except
-import Control.Monad.IO.Class (liftIO)
-import Text.Parsec hiding ((<|>))
-import Text.Parsec.String (Parser)
-import Control.Applicative ((<|>))
-import Network.HTTP.Simple
-import qualified Data.ByteString.Lazy as BL
-import Data.Aeson hiding (encode)
-import Network.URI.Encode (encode)
+import           Control.Applicative        ((<|>))
+import           Control.Monad              (unless)
+import           Control.Monad.IO.Class     (liftIO)
+import           Control.Monad.Trans.Except
+import           Data.Aeson                 hiding (encode)
+import qualified Data.Text                  as T
+import           Network.HTTP.Simple
+import           Network.URI.Encode         (encode)
+import           System.Directory           (doesFileExist, renameFile)
+import           System.Environment         (getArgs)
+import           System.Exit                (die)
+import           System.FilePath
+import           Text.Parsec                hiding ((<|>))
+import           Text.Parsec.String         (Parser)
 
 data Command = Curate | Tag
     deriving (Show, Eq)
 
 parseCommand :: String -> Maybe Command
 parseCommand "curate" = Just Curate
-parseCommand "tag" = Just Tag
-parseCommand _ = Nothing
+parseCommand "tag"    = Just Tag
+parseCommand _        = Nothing
 
-data MP3Info = MP3Info 
-    { songName :: String
+data MP3Info = MP3Info
+    { songName   :: String
     , authorName :: String
     } deriving Show
 
-data ValidationError 
+data ValidationError
     = NotAbsolutePath
     | NoParentFolder
     | NotMP3Extension
@@ -40,8 +39,8 @@ data ValidationError
     deriving Show
 
 data KbpsPattern = KbpsPattern
-    { openBracket :: Char
-    , bitrate :: Int
+    { openBracket  :: Char
+    , bitrate      :: Int
     , closeBracket :: Char
     } deriving Show
 
@@ -85,9 +84,9 @@ songNameParser = do
     return (name, kbps)
 
 replaceKbpsPattern :: String -> String
-replaceKbpsPattern input = 
+replaceKbpsPattern input =
     case parse songNameParser "" input of
-        Left _ -> input  -- if parsing fails, return original input
+        Left _          -> input  -- if parsing fails, return original input
         Right (name, _) -> trim name
   where
     trim = T.unpack . T.strip . T.pack
@@ -96,9 +95,9 @@ curateMP3 :: MP3Info -> FilePath -> IO ()
 curateMP3 mp3info originalPath = do
     let oldName = songName mp3info
         -- Remove patterns like (XXX Kbps), [XXX Kbps], etc
-        newName = T.unpack $ T.replace "  " " " $ 
-                   T.strip $ 
-                   T.pack $ 
+        newName = T.unpack $ T.replace "  " " " $
+                   T.strip $
+                   T.pack $
                    replaceKbpsPattern oldName
         -- Create new path by replacing old filename with curated one
         directory = takeDirectory originalPath
@@ -127,32 +126,42 @@ validatePath path = runExceptT $ do
     let dirs = splitDirectories path
         fileName = takeBaseName (last dirs)
         author = last (init dirs)
+
     return $ MP3Info fileName author
 
 validateArgs :: [String] -> Either CommandLineError ValidatedArgs
 validateArgs [] = Left NoArgs
 validateArgs [_] = Left NoFilePath
-validateArgs (cmd:filePath:_) = 
+validateArgs (cmd:filePath:_) =
     case parseCommand cmd of
-        Nothing -> Left InvalidCommand
+        Nothing      -> Left InvalidCommand
         Just command -> Right $ ValidatedArgs command filePath
 
 executeCommand :: ValidatedArgs -> IO ()
-executeCommand (ValidatedArgs command filePath) = 
-    case command of
-        Curate -> do
-            result <- validatePath filePath
-            case result of
-                Left err -> die $ "Error: " ++ showValidationError err
-                Right mp3Info -> curateMP3 mp3Info filePath
-        Tag -> putStrLn "TBD: Tag file"
+executeCommand (ValidatedArgs command filePath) = do
+    result <- validatePath filePath
+    case result of
+        Left err      -> die $ "Error: " ++ showValidationError err
+        Right mp3Info ->
+          case command of
+              Curate -> curateMP3 mp3Info filePath
+              Tag -> do
+                x <- searchMusicBrainz (songName mp3Info) (authorName mp3Info)
+                case x of
+                    Left err -> die $ "Error: " ++ err
+                    Right track -> do
+                        putStrLn $ "Title: " ++ mbTitle track
+                        putStrLn $ "Artist: " ++ mbArtist track
+                        putStrLn $ "Release Date: " ++ mbReleaseDate track
+                        putStrLn $ "Genres: " ++ unwords (mbGenres track)
+                putStrLn "TBD: Tag file"
 
 -- MusicBrainz API
 data MusicBrainzTrack = MusicBrainzTrack
-    { mbTitle :: String
-    , mbArtist :: String
+    { mbTitle       :: String
+    , mbArtist      :: String
     , mbReleaseDate :: String
-    , mbGenres :: [String]
+    , mbGenres      :: [String]
     } deriving (Show, Eq)
 
 instance FromJSON MusicBrainzTrack where
@@ -162,23 +171,23 @@ instance FromJSON MusicBrainzTrack where
             [] -> fail "No recordings found"
             (recording:_) -> do
                 title <- recording .: "title"
-                
+
                 -- Get artist from artist-credit array
                 artistCredits <- recording .: "artist-credit"
                 artist <- case artistCredits of
-                    [] -> fail "No artist credits found"
+                    []         -> fail "No artist credits found"
                     (credit:_) -> credit .: "name"
-                
+
                 -- Get release date from releases array
                 releases <- recording .:? "releases" .!= []
                 releaseDate <- case releases of
-                    [] -> return "Unknown"
+                    []          -> return "Unknown"
                     (release:_) -> release .:? "date" .!= "Unknown"
-                
+
                 -- Get genres from tags array
                 tags <- recording .:? "tags" .!= []
                 genres <- sequence $ map (\tag -> tag .: "name") tags
-                
+
                 return $ MusicBrainzTrack
                     { mbTitle = title
                     , mbArtist = artist
@@ -190,20 +199,19 @@ searchMusicBrainz :: String -> String -> IO (Either String MusicBrainzTrack)
 searchMusicBrainz title artist = do
     let query = encode $ "recording:" ++ title ++ " AND artist:" ++ artist
         url = "https://musicbrainz.org/ws/2/recording/?query=" ++ query ++ "&fmt=json"
-    
+
     request <- parseRequest url
-    let requestWithAgent = addRequestHeader "User-Agent" "YourApp/1.0" request
-    
+
     response <- httpLBS request
     return $ case getResponseStatusCode response of
         200 -> case decode (getResponseBody response) of
             Just track -> Right track
-            Nothing -> Left "Failed to parse MusicBrainz response"
-        code -> Left $ "MusicBrainz API error: " ++ show code
+            Nothing    -> Left "Failed to parse MusicBrainz response"
+        code -> Left $ "MusicBrainz API error: " ++ show code ++ show (getResponseBody response)
 
 main :: IO ()
 main = do
     args <- getArgs
     case validateArgs args of
-        Left err -> die $ "Error: " ++ showCommandLineError err
+        Left err            -> die $ "Error: " ++ showCommandLineError err
         Right validatedArgs -> executeCommand validatedArgs
